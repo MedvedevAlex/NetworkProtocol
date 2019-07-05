82c89a7a87a06cc186b9da75f7142613854ab4ad
Alex Medvedev <localisseat@gmail.com>
2019-07-05 21:48:51 +0700

    Add project files.

---
+++ NetworkProtocol.Tests/MessageWindowTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProtocol.Tests
+{
+    [TestFixture]
+    public class MessageWindowTests
+    {
+        [TestCase(10,1,10)]
+        [TestCase(20,1,20)]
+        public void LowAndHighBoundatiesInitiallySet(int size, int low, int high)
+        {
+            MessageWindow window = new MessageWindow(size);
+            Assert.That(window.LowBoundaryId, Is.EqualTo(low));
+            Assert.That(window.HighBoundaryId, Is.EqualTo(high));
+
+        }
+    }
+}
---
+++ NetworkProtocol/MessageWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProtocol
+{
+    public class MessageWindow
+    {
+        private readonly int _size;
+
+        public MessageWindow(int size)
+        {
+            _size = size;
+        }
+
+        public int HighBoundaryId { get { return _size; } }
+        public int LowBoundaryId { get { return 1; } }
+    }
+}
