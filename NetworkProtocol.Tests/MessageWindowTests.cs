d1e6d139cb0c98f9bef217ef30179e901ac7208f
Alex Medvedev <localisseat@gmail.com>
2019-07-05 22:10:29 +0700

    Remove
    added remove method
    FillWindow and ShiftWindow for moving window

--- NetworkProtocol.Tests/MessageWindowTests.cs
+++ NetworkProtocol.Tests/MessageWindowTests.cs
@@ -14,10 +14,25 @@ namespace NetworkProtocol.Tests
         [TestCase(20,1,20)]
         public void LowAndHighBoundatiesInitiallySet(int size, int low, int high)
         {
+            //Arrange
             MessageWindow window = new MessageWindow(size);
+            //Act
+
+            //Assert
             Assert.That(window.LowBoundaryId, Is.EqualTo(low));
             Assert.That(window.HighBoundaryId, Is.EqualTo(high));
 
+        }
+        [Test]
+        public void LowBoundaryIdIsRemoved_WindowIsShifted()
+        {
+            //Arrange
+            MessageWindow window = new MessageWindow(15);
+            //Act
+            window.Remove(1);
+            //Assert
+            Assert.That(window.LowBoundaryId, Is.EqualTo(2));
+            Assert.That(window.HighBoundaryId, Is.EqualTo(16));
         }
     }
 }
--- NetworkProtocol/MessageWindow.cs
+++ NetworkProtocol/MessageWindow.cs
@@ -8,14 +8,36 @@ namespace NetworkProtocol
 {
     public class MessageWindow
     {
+        public int HighBoundaryId => _window[_window.Count - 1];
+        public int LowBoundaryId => _window[0];
+
+        private readonly List<int> _window;
         private readonly int _size;
 
         public MessageWindow(int size)
         {
             _size = size;
+            _window = new List<int>(size);
+            FillWindow();
         }
 
-        public int HighBoundaryId { get { return _size; } }
-        public int LowBoundaryId { get { return 1; } }
+        public void Remove(int id)
+        {
+            _window.Remove(id);
+            ShiftWindow();
+        }
+
+        private void FillWindow()
+        {
+            for (int i = 1; i <= _size; i++)
+            {
+                _window.Add(i);
+            }
+        }
+
+        private void ShiftWindow()
+        {
+            _window.Add(LowBoundaryId + _size - 1);
+        }
     }
 }
